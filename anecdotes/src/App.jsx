f565032ea703f065c9a306383b58b5a71baf010b
Abhishekadhikari725 <abhiadk725@gmai.com>
2023-12-30 16:54:01 +0300

    part 1 submission

---
+++ anecdotes/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+
+
+const Title = ({text}) => {
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
+}
+
+const Anecdotes = ({anecdotes,vote}) => {
+  return(
+    <>
+      <div>{anecdotes}</div>
+      <div>has {vote} votes</div>
+    </>
+  )
+}
+
+const Button = ({onclick,text}) => {
+  return (
+    <button onClick={onclick}>{text}</button>
+  )
+}
+
+const VoteOfday = ({anecdotes,high}) => {
+  return (
+    <>
+      <p>{anecdotes}
+      <br />
+      has {high} votes</p>
+    </>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(new Array(anecdotes.length).fill(0))
+
+  const next_handler = () => {
+    const newIndex = selected + 1
+    setSelected(newIndex < anecdotes.length ? newIndex : 0)
+  }
+
+  const vote_handler = () => {
+    const newVotes = [...vote]
+    newVotes[selected] += 1
+    setVote(newVotes)
+  }
+
+  
+  const highest_vote = Math.max(...vote)
+
+  const index_high = vote.indexOf(highest_vote)
+
+
+  return (
+    <div>
+      <Title text='Anecdote of the day'/>
+      <Anecdotes anecdotes={anecdotes[selected]} vote={vote[selected]}/>
+      <Button onclick={next_handler} text='next anecdote'/>
+      <Button onclick={vote_handler} text='vote'/>
+      <Title text='Anecdote with most votes'/>
+      <VoteOfday anecdotes={anecdotes[index_high]} high={highest_vote} />
+    </div>
+  )
+}
+
+export default App;
